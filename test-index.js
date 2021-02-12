802eb87739981a1e67be608a41c9db93c77fea9d
raksa <eng.raksa@gmail.com>
2021-02-12 17:40:32 +0700

    add test

--- test-index.js
+++ test-index.js
@@ -1,4 +1,8 @@
 const { KhmerChess } = require("./index");
 
-const kc = new KhmerChess();
+// const kc = new KhmerChess('bhgqkghb/8/1ff5/8/8/FFFFFFFF/8/BHGKQGHB w ---- -- -.- ffffff');
+const kc = new KhmerChess('4k3/8/8/8/8/8/8/3K4 w ---- -- -.- bhgqghbffffffffFFFFFFFFBHGQGHB');
+// console.log(kc.ren());
 console.log(kc.ascii());
+// console.log(kc.board());
+// console.log(kc.validate_ren('bhgqkghb/8/1ff5/8/8/FFFFFFFF/8/BHGKQGHB w ---- -- -.- ffffffK'));
