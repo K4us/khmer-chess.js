b69280b28cc6d73f8b1d9f5d49644e5f8f3b20d1
raksa <eng.raksa@gmail.com>
2021-02-14 22:24:45 +0700

    release 1.0.8

--- test-index.js
+++ test-index.js
@@ -1,8 +1,9 @@
 const { KhmerChess } = require("./index");
 
 // const kc = new KhmerChess('bhgqkghb/8/1ff5/8/8/FFFFFFFF/8/BHGKQGHB w ---- -- -.- ffffff');
-const kc = new KhmerChess('4k3/8/8/8/8/8/8/3K4 w ---- -- -.- bhgqghbffffffffFFFFFFFFBHGQGHB');
+// const kc = new KhmerChess('4k3/8/8/8/8/8/8/3K4 w ---- -- -.- bhgqghbffffffffFFFFFFFFBHGQGHB');
+const kc = new KhmerChess('bhgqkghb/8/1tf5/8/8/FFFFFFFF/8/BHGKQGHB w ---- -- -.- ffffff');
 // console.log(kc.ren());
-// console.log(kc.ascii());
-console.log(kc.board());
+console.log(kc.ascii());
+// console.log(kc.board());
 // console.log(kc.validate_ren('bhgqkghb/8/1ff5/8/8/FFFFFFFF/8/BHGKQGHB w ---- -- -.- ffffffK'));
