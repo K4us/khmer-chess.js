8c9378cc5d0a504777cda11dac2424d23f0a53ca
raksa <eng.raksa@gmail.com>
2021-02-18 15:33:41 +0700

    fixed

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,5 +2,5 @@ const KhmerChess = require('../index').KhmerChess;
 
 test('KhmerChess', () => {
     const kChess = new KhmerChess();
-    expect(kChess.game_over()).toBe(false);
+    expect(kChess.gameOver()).toBe(false);
 });
